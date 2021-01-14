c20a41159d6e04d20cf1273c8250999aa80b1e20
emmachio <echio20@stuy.edu>
2021-01-14 02:20:27 -0500

    fixed nth and did radixSortSimple

--- Radix.java
+++ Radix.java
@@ -5,7 +5,7 @@ public class Radix{
 
   public static int nth(int n, int col) {
     n = n / ((int)Math.pow(10,col));
-    return Math.abs(n % ((int)Math.pow(10,col)));
+    return Math.abs(n % 10);
   }
 
   public static int length(int n) {
@@ -26,11 +26,31 @@ public class Radix{
     }
   }
 
+  public static String radixSortSimple(SortableLinkedList data) {
+    int maxlength = 1;
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+    for(int i = 0; i < maxlength; i++) {
+      for(int j = 0; j < data.size(); j++) {
+        if(length(data.get(j)) > maxlength) {
+          maxlength = length(data.get(j));
+        }
+        int value = nth(data.get(j), i);
+        buckets[value].add(data.get(j));
+      }
+      data = new SortableLinkedList();
+      merge(data, buckets);
+    }
+    return data.toString();
+  }
+
   public static void main(String[] args) {
-    System.out.println(length(0));
-    System.out.println(length(15));
-    System.out.println(length(-10));
-    System.out.println(length(5112));
+    SortableLinkedList test = new SortableLinkedList();
+    test.add(600); test.add(13); test.add(4); test.add(201); test.add(11);
+    System.out.println(radixSortSimple(test));
+    // System.out.println(nth(4321, 0));
   }
 
 }
