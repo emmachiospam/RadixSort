a327393b55446182fe5e4528265d681569cec039
emmachio <echio20@stuy.edu>
2021-01-15 10:18:44 -0500

    fixed RadixSimple and Radix

--- Radix.java
+++ Radix.java
@@ -26,57 +26,62 @@ public class Radix{
     }
   }
 
-  public static String radixSortSimple(SortableLinkedList data) {
+  public static void radixSortSimple(SortableLinkedList data) {
     int maxlength = 0;
     SortableLinkedList[] buckets = new SortableLinkedList[10];
     for (int i = 0; i < 10; i++) {
       buckets[i] = new SortableLinkedList();
     }
-    for(int i = 0; i < data.size(); i++) {
+    int size = data.size();
+    for(int i = 0; i < size; i++) {
       maxlength = Math.max(maxlength, Math.abs(data.get(0)));
       data.add(data.remove(0));
     }
     maxlength = length(maxlength);
     for(int i = 0; i < maxlength; i++) {
-      for(int j = 0; j < data.size(); j++) {
+      for(int j = 0; j < size; j++) {
         int value = nth(data.get(0), i);
         buckets[value].add(data.get(0));
         data.remove(0);
       }
       merge(data, buckets);
     }
-    return data.toString();
   }
 
-  // public static void radixSort(SortableLinkedList data) {
-  //   int maxlength = 1;
-  //   SortableLinkedList[] buckets = new SortableLinkedList[20];
-  //   for (int i = 0; i < 20; i++) {
-  //     buckets[i] = new SortableLinkedList();
-  //   }
-  //   for(int i = 0; i < maxlength; i++) {
-  //     for(int j = 0; j < data.size(); j++) {
-  //       if(length(data.get(j)) > maxlength) {
-  //         maxlength = length(data.get(j));
-  //       }
-  //       if(data.get(j) < 0) {
-  //         int value = (nth(data.get(j), i));
-  //         buckets[9 - value].add(data.get(j));
-  //       }
-  //       else{
-  //         int value2 = nth(data.get(j), i) + 10;
-  //         buckets[value2].add(data.get(j));
-  //       }
-  //     }
-  //     data = new SortableLinkedList();
-  //     merge(data, buckets);
-  //   }
+
+
+  public static void radixSort(SortableLinkedList data) {
+    int maxlength = 0;
+    SortableLinkedList[] buckets = new SortableLinkedList[20];
+    for (int i = 0; i < 20; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+    int size = data.size();
+    for(int i = 0; i < size; i++) {
+      maxlength = Math.max(maxlength, Math.abs(data.get(0)));
+      data.add(data.remove(0));
+    }
+    maxlength = length(maxlength);
+    for(int i = 0; i < maxlength; i++) {
+      for(int j = 0; j < size; j++) {
+        if(data.get(0) < 0) {
+          int value = nth(data.get(0), i);
+          buckets[9 - value].add(data.get(0));
+          data.remove(0);
+        }
+        else{
+          int value2 = nth(data.get(0), i) + 10;
+          buckets[value2].add(data.get(0));
+          data.remove(0);
+        }
+      }
+      merge(data, buckets);
+    }
+  }
+
+  // public static void main(String[] args) {
+  //   SortableLinkedList test = new SortableLinkedList();
+  //   test.add(2); test.add(-300); test.add(112);  test.add(100);
+  //   System.out.println((radixSort(test)));
   // }
-
-  public static void main(String[] args) {
-    SortableLinkedList test = new SortableLinkedList();
-    test.add(2); test.add(300); test.add(112);  test.add(100);
-    System.out.println((radixSortSimple(test)));
-    System.out.println(nth(112, 0));
-  }
 }
